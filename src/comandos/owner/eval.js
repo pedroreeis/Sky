beb66373c331d2a3f9f0126d6b403aa01d7e136d
Pedro de Andrade Reis <pegasusprocsgo@gmail.com>
2020-11-13 12:12:44 -0300

    realese 13/11/2020 1.9

--- src/comandos/owner/eval.js
+++ src/comandos/owner/eval.js
@@ -8,12 +8,12 @@ module.exports = {
     run: async (client, message, args, db) => {
       if (message.author.id == "640195412648788018") {
     try {
-        if(!args.join(' ')) return message.channel.send(`<@${message.author.id}> Insira algo para ser executado!`)
+        if(!args.join(' ')) return message.channel.send(`Insira algo para ser executado!`)
         let code = eval(args.join(" "))
         
         if (typeof code !== 'string') code = require('util').inspect(code, { depth: 0 });
         let embed = new Discord.MessageEmbed()
-        .setDescription('Eval')
+        .setDescription('Vamos ver o que você tem pa nois!')
         .addField('Entrada', `\`\`\`js\n${args.join(" ")}\`\`\``)
         .addField('Saída', `\`\`\`js\n${code}\n\`\`\``)
         message.channel.send(embed)
