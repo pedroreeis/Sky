3852e95cb132b78c34c4e8fd1f0be039d2206b2e
Pedro de Andrade Reis <pegasusprocsgo@gmail.com>
2020-11-18 12:36:08 -0300

    hastebin kk

--- src/comandos/owner/supereval.js
+++ src/comandos/owner/supereval.js
@@ -1,22 +1,20 @@
 const Discord = require('discord.js')
 module.exports = {
-    name: "eval",
+    name: "supereval",
     category: "Owner",
-    usage: 'eval [code]',
-    aliases: ['execute', 'e'],
+    usage: 'supereval [code]',
+    aliases: ['superexecute', 'se'],
     description: "Comando privado",
     run: async (client, message, args, db) => {
+        const hastebin = require('hastebin')
       if (message.author.id == "640195412648788018") {
     try {
         if(!args.join(' ')) return message.channel.send(`Insira algo para ser executado!`)
         let code = eval(args.join(" "))
         
         if (typeof code !== 'string') code = require('util').inspect(code, { depth: 0 });
-        let embed = new Discord.MessageEmbed()
-        .setDescription('Vamos ver o que você tem pa nois!')
-        .addField('Entrada', `\`\`\`js\n${args.join(" ")}\`\`\``)
-        .addField('Saída', `\`\`\`js\n${code}\n\`\`\``)
-        message.channel.send(embed)
+        let a = hastebin.createPaste(code)
+        message.author.send(a)
     } catch(e) {
         message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
     }
